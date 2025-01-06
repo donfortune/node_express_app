f1aa7c37e9a4b8269150f8067f8c5e3ef83e67f0
donfortune <donfortunet.df@gmail.com>
2025-01-06 15:55:14 +0100

    connected mongodb to project using Mongoose

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-
+const mongoose = require('mongoose')
 const dotenv = require("dotenv")
 dotenv.config({ path: './config.env' })
 const app = require('./app')
 
+dotenv.config({ path: './config.env' })
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+mongoose
+    // .connect(process.env.DATABASE_LOCAL, { // .connect(DB, {)
+    .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then((con) => {
+    console.log(con.connections)
+    console.log('DB connection successful')
+})
 
 console.log(app.get('env')) //gets the environment variable
 // console.log(process.env) //gets all the environment variables
@@ -11,3 +25,4 @@ const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`App is listening at port ${port}....`)
 })
+
