2414f1c989828042ff9293a81028cc66a8db8a42
donfortune <donfortunet.df@gmail.com>
2025-01-07 16:15:38 +0100

    completed crud implementation with update and delete

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -100,31 +100,51 @@ exports.createTour = async (req, res) => {
     
 }
 
-exports.updateTour = (req, res) => {
-    const tour = tours.find(element => element.id === parseInt(req.params.id))
+exports.updateTour = async (req, res) => {
+    // const tour = tours.find(element => element.id === parseInt(req.params.id))
     // if (!tour) {
     //     return res.status(404).json({
     //         status: 'fail',
     //         message: 'Invalid ID'
     //     })
     // }
-    updatedTour = Object.assign(tour, req.body);
-    res.status(200).json({
-        status: 'success',
-        data: {
-            updatedTour
-        }
-    })
+    // updatedTour = Object.assign(tour, req.body);
+    try {
+        const tour = await Tour.findByIdAndUpdate(req.params.id , req.body, {
+            new: true,
+            runValidators: true
+        })
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour
+            }
+        })
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err
+        })
+    }
 }
 
-exports.deleteTour = (req, res) => {
-    const tour = tours.find(element => element.id === parseInt(req.params.id))
+exports.deleteTour = async (req, res) => {
+    try {
+        const tour = await Tour.findByIdAndDelete(req.params.id)
+        res.status(204).json({
+            status: 'success',
+            data: null
+        })
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+    }
+    // const tour = tours.find(element => element.id === parseInt(req.params.id))
     
-    const index = tours.indexOf(tour)
-    console.log(index)
-    tours.splice(index, 1)
-    res.status(204).json({
-        status: 'success',
-        data: null
-    })
+    // const index = tours.indexOf(tour)
+    // console.log(index)
+    // tours.splice(index, 1)
+    
 }
