5be1c1464d4b6a375389556f7ef674fc70d7bea0
donfortune <donfortunet.df@gmail.com>
2025-01-08 17:27:22 +0100

    complete modeling of tour schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,17 +8,59 @@ const tourSchema = new mongoose.Schema({
         unique: true,
         trim: true
     },
-    rating: {
+    duration: {
+        type: Number,
+        required: [true, 'A tour must have a duration']
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A tour must have a group size']
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must have a difficulty']
+    },
+    ratingsAverage: {
         type: Number,
         default: 4.5,
         min: [1, 'Rating must be above 1.0'],
         max: [5, 'Rating must be below 5.0']
     },
+    ratingsQuantity: {
+        type: Number,
+        default: 0
+    },
     price: {
         type: Number,
         required: [true, 'A tour must have a price'],
         min: [0, 'Price must be above 0']
-    }
+    },
+    priceDiscount: {
+        type: Number
+    },
+    summary: {
+        type: String,
+        trim: true,
+        required: [true, 'A tour must have a summary']
+    },
+    description: {
+        type: String,
+        trim: true,
+        required: [true, 'A tour must have a description']
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'A tour must have a cover image']
+    },
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+        select: false
+    },
+    startDates: [Date]
+
+
 });
 
 // create model from schema
